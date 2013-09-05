1dfbfc2fb38ef7713f75b53ca9af2afeaf1ba816
Seyar Chapuh <seyarchapuh@ya.ru>
2013-09-05 12:37:38 +0300

    Накидана структура. установлен inherit

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-require();
+var inherit = require('inherit');
+
+var IAuthirzation = inherit({
+    __constructor: function(cloud){
+        this.cloud = cloud;
+    },
+
+    /**
+     * Function get token from cloud
+     */
+    getToken: function(){
+    }
+});
+
+var YaDiskAuthorization = inherit(IAuthirzation,{
+
+});
+
+
+var IConfigReader = inherit({
+    __constructor: function(){
+        /**
+         * Driver for read config
+         */
+        this.reader = FileConfigReader;
+
+        /**
+         * Config values
+         */
+        this.config = {};
+    },
+
+    /**
+     * Read all filename
+     * @return {Object} JSON
+     *
+    read: function(){
+    },
+    */
+
+    /**
+     * Function get one config value
+     * @param {String}  config key
+     * @return
+     */
+    get: function(key){
+
+    },
+
+    /**
+     * Function get all config values
+     * @return {Object} JSON
+     */
+    getConfig: function(){
+        return this.config;
+    }
+});
+
+var FileConfigReader = inherit(IConfigReader,{
+
+});
