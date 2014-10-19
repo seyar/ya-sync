790f7d3cea7f70d6e7c6d89c65c8018f129125f2
Seyar Chapuh <seyar@yandex-team.ru>
2014-10-19 15:49:14 +0400

    added lints and tests

---
+++ database/read.test.js
@@ -0,0 +1,23 @@
+describe('read', function () {
+
+    beforeEach(function () {
+
+    });
+    afterEach(function () {
+
+    });
+
+    describe('js', function () {
+        describe('constructor', function () {
+            it('should …', function () {
+
+            });
+        });
+    });
+
+    describe('bt', function () {
+        it('', function () {
+
+        });
+    });
+});
